e9e07d0f9984dca9060ed91d1f6c05e53c833de4
kubrakurukamci <kubratrh1453@gmail.com>
2021-09-11 21:43:04 +0200

    My  second commit to feature B

--- src/test/java/gitProject/FeatureB.java
+++ src/test/java/gitProject/FeatureB.java
@@ -3,5 +3,6 @@ package gitProject;
 public class FeatureB {
     public static void main(String[] args) {
         System.out.println("I am developing some new stuff");
+        System.out.println("some work added");
     }
 }
