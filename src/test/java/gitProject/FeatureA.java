00ce835e36d280d075f224b00b4dca5bea0aa58c
kubrakurukamci <kubratrh1453@gmail.com>
2021-09-11 21:35:20 +0200

    My second commit to feature A

--- src/test/java/gitProject/FeatureA.java
+++ src/test/java/gitProject/FeatureA.java
@@ -3,6 +3,7 @@ package gitProject;
 public class FeatureA {
     public static void main(String[] args) {
         System.out.println("I made same work on feature A" );
+        System.out.println("more work");
     }
 
 }
